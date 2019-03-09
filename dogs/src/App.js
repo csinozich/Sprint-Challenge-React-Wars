55da0c4a7034d3b2462dd25301dbe6d927937098
Claire Sinozich <claire.sinozich@gmail.com>
2019-03-09 11:04:43 -0700

    more setup

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
-import Character from './components/Dogs'
+import Dogs from './components/Dogs'
 import './components/Dogs.css'
 
 class App extends Component {
@@ -16,7 +15,7 @@ class App extends Component {
 );
   }
 
-  getCharacters = URL => {
+  getDogs = URL => {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
@@ -36,7 +35,11 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">Dog Breeds</h1>
-
+        <div className='Dog'>
+          {this.state.dogBreeds.map(dog => (
+            <Dogs dog={dog} key={this.state.dogBreeds.index} dogBreedInfo = {this.state.dogBreeds} />
+          ))}
+        </div>
       </div>
     );
   }
